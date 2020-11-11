11c544cded668c77073912426aaf8c59602110c4
Ryan McClure <ryan333@vt.edu>
2020-11-11 09:33:15 -0500

    glm updates

--- scripts/Run_GLM.R
+++ scripts/Run_GLM.R
@@ -10,9 +10,7 @@
 
 # Get packages and specify sites to download
 # -----------------------------------------------------------------------------------------------------------------
-remotes::install_github("CareyLabVT/GLMr")
 remotes::install_github("CareyLabVT/glmtools")
-remotes::install_github("GLEON/GLM3r")
 
 download.file("https://aquatic.science.uwa.edu.au/research/models/GLM/Download/downloader.php?bin&version=3.1/glm_3.1.0a4-0_amd64.deb.gz", destfile = "./glm_3.1.0a4-0_amd64.deb.gz")
 
@@ -36,7 +34,22 @@ nc_file <- file.path(sim_folder, 'output/output.nc') #defines the output.nc file
 field_file<-file.path('./observations/CleanedObsTempBARC.csv')
 plot_temp_compare(nc_file, field_file)
 
+EPI_RMSE <- compare_to_field(nc_file, field_file, nml_file = nml_file, metric = 'epi.temperature', as_value = F,
+                 na.rm = TRUE, precision = 'days',method = 'interp')
+
+temps <- resample_to_field(nc_file, field_file, precision="mins", method='interp')
+
+temps<-temps[complete.cases(temps),]
+
+temps_1 <- temps %>% filter(Depth == 1.05)
+plot(temps_1$DateTime, temps_1$Observed_temp, col = "black", main = " 1m Temperature Mod = red; Obs = black")
+points(temps_1$DateTime, temps_1$Modeled_temp, col = "red")
+
+
+
 #get water level
 water_level<-get_surface_height(nc_file, ice.rm = TRUE, snow.rm = TRUE)
 plot(water_level$DateTime,water_level$surface_height, ylim = c(5,9), main = "Barco!")
 lines(as.POSIXct(water_level_barc$DateTime),water_level_barc$surface_height, type = "p", ylim = c(5,9), col = "red")
+
+
